d33409129c072d1f98ab42da81835bd9d62ac52e
marksult <marksult767@gmail.com>
2026-05-14 20:34:44 +0000

    14 трав. 2026 р., 23:34

--- assets/js/cases-sticky.js
+++ assets/js/cases-sticky.js
@@ -4,14 +4,12 @@
   function updateCardTops() {
     var vh    = window.innerHeight;
     var cards = document.querySelectorAll('.cases__card');
-
-    /* Read all heights first (one reflow) */
     var heights = [];
+
     cards.forEach(function (card) {
       heights.push(card.getBoundingClientRect().height);
     });
 
-    /* Write all tops after (no extra reflow) */
     cards.forEach(function (card, i) {
       var top = vh - heights[i] - CARD_OFFSET;
       card.style.top = Math.min(top, 0) + 'px';
